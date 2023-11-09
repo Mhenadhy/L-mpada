97da24724721246e10b840bb85bda32f0998b9be
mhenadhy <mhenadhyesperanca@gmail.com>
2023-11-09 19:10:11 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let imagem = document.querySelector("img")
+
+function ligar(){
+    imagem.src = "./ligada.png"
+    imagem.style.height = "20vw"
+}
+
+function desligar(){
+    imagem.src = "./desligada01.png"
+    imagem.style.height = "20vw"
+
+}
